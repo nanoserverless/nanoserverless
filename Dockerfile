32c9fdf4d0473089489f51c89754773c1b9aaaf9
WTFKr0 <thomas.kovatchitch@gmail.com>
2017-02-03 23:08:48 +0100

    Init

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM    node:7
+WORKDIR /usr/src/app
+COPY    package.json /usr/src/app
+RUN     npm install
+COPY    . /usr/src/app
+CMD     ["npm","start"]
---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var morgan = require('morgan');
+var querystring = require('querystring');
+
+var app = express();
+
+app.use(morgan('combined'));
+
+app.get('/function', function (req, res) {
+  res.send('NanoServerLess')
+})
+
+app.listen(3000, function () {
+  console.log('NanoServerLess started on port 3000');
+});
